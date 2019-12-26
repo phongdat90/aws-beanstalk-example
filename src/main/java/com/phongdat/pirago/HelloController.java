7c7ade190852d71447ee29b2b592388134da508c
Phong Dat <phongdat.pirago@gmail.com>
2019-12-26 11:17:52 +0700

    edit HelloController

--- src/main/java/com/phongdat/pirago/HelloController.java
+++ src/main/java/com/phongdat/pirago/HelloController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 @RestController
 public class HelloController {
+	@GetMapping
 	public String hello() {
 		return "Hello youtube";
 	}
