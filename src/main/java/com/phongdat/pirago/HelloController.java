986b55d1056aca870c95479090a26bbd8f80f082
Phong Dat <phongdat.pirago@gmail.com>
2019-12-26 11:57:20 +0700

    all

--- src/main/java/com/phongdat/pirago/HelloController.java
+++ src/main/java/com/phongdat/pirago/HelloController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@GetMapping
 	public String hello() {
-		return "Hello youtube";
+		return "Aws BeanStalk CodePipeline";
 	}
 }
